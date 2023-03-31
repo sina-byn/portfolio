900dd6aaba336673a207f81f8222078db3f22893
sina-byn <sina.sina.bayan@protonmail.com>
2023-03-31 18:20:56 +0430

    hero section added

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ import Header from './components/Header';
 import TechStack from './components/TechStack';
 import Projects from './components/Projects';
 import Footer from './components/Footer';
+import HeroSection from './components/HeroSection';
 
 const App = () => {
   return (
     <main className='app-container'>
       <Header />
       <Container>
+        <HeroSection />
         <TechStack />
         <Projects />
         <Footer />
