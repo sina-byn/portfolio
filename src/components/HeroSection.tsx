ac8b69af8a9fe818df3539d7fa0f6f9136f33ca0
sina-byn <sina.sina.bayan@protonmail.com>
2023-04-21 00:48:28 +0430

    hero section made responsive

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,21 +1,21 @@
 // * components
-import Circuit from './Circuit';
 import Container from './Container';
+import Circuit from './Circuit';
 
 const HeroSection = () => {
   return (
     <Container>
       <section
         id='home'
-        className='hero-section grid sm:flex grid-rows-2 grid-cols-2 flex-row sm:items-center pt-24 sm:pl-6'
+        className='hero-section grid sm:flex grid-rows-2 grid-cols-2 gap-x-10 sm:items-center pt-24 sm:pl-6 mb-16 sm:mb-0'
       >
-        <figure className='image-container col-span-2 grid place-items-center w-80 h-96 relative bg-radial-gradient mx-auto sm:mx-0'>
+        <figure className='image-container col-span-2 grid place-items-center min-w-[280px] w-2/4 sm:w-80 sm:h-96 relative bg-radial-gradient mx-auto sm:mx-0'>
           <img
-            src='/arrow.svg'
             alt='arrow'
+            src='/arrow.svg'
             className='hidden sm:block absolute top-5 right-5'
           />
-          <img src='/memoji.png' alt='memoji' className='w-32' />
+          <img src='/memoji.png' alt='memoji' className='w-[40%] sm:w-32' />
           <div className='backdrop-light bg-radial-gradient w-full h-full absolute -z-10' />
         </figure>
         <div className='flex flex-col grow sm:self-start pl-6 sm:pl-0'>
